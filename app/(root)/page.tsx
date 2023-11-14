033140d955798653691e27fa55ab6d1bc60a7c91
Yerai <justharden1@gmail.com>
2023-11-14 12:34:57 +0100

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { Modal } from '@/components/ui/modal'
+import { useStoreModal } from '@/hooks/use-store-modal'
+import { Children, useEffect } from 'react'
+
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [isOpen, onOpen])
+
+  return <div className="p-4">Root page</div>
+}
+
+export default SetupPage
